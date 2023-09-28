9fbf0dc73632b3ea3d387c83d1fecff92a2114b2
sami903 <146079758+sami903@users.noreply.github.com>
2023-09-28 12:42:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function varKeyword(){
+    if(true){
+        var a="Virat";
+        console.log(a);
+        a="kholi";
+        console.log(a);
+        a="myfavourite";
+        console.log(a);
+        a="RCB";
+        
+    }
+    console.log(a);
+}
+varKeyword();
+
+
+function letKeyword(){
+    
+    if(true){
+        let b="Msdhoni";
+        console.log(b);
+        b="Finisher";
+        console.log(b);
+    
+ }
+}
+letKeyword();
+
+
+function constKeyword(){
+    if(true){
+        const s="Klrahul";
+        console.log(s)
+    }
+}
+constKeyword();
